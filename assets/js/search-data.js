f1eeef6417cb17081c3d062383fab611e00c56e8
vanessa-lazaro <lazaro@uchicago.edu>
2025-10-14 16:31:37 +0000

    Deploying to gh-pages from @ vanessa-lazaro/vanessa-lazaro.github.io@30960e7044bf571abeb576d6a0ccabfff2bba00f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,9 +16,9 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
+        },{id: "nav-current-projects",
+          title: "current projects",
+          description: "what research am I currently working on?",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
