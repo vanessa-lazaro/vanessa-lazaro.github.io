f8465397c021e01de32eadc38dedd6dbaec1e9c4
vanessa-lazaro <lazaro@uchicago.edu>
2025-10-17 20:52:25 -0500

    Update common.js

--- assets/js/common.js
+++ assets/js/common.js
@@ -37,10 +37,19 @@ $(document).ready(function () {
   cssLink.rel = "stylesheet";
   cssLink.type = "text/css";
 
+  let jupyterTheme = determineComputedTheme();
+
   $(".jupyter-notebook-iframe-container iframe").each(function () {
     $(this).contents().find("head").append(cssLink);
 
- 
+    if (jupyterTheme == "dark") {
+      $(this).bind("load", function () {
+        $(this).contents().find("body").attr({
+          "data-jp-theme-light": "false",
+          "data-jp-theme-name": "JupyterLab Dark",
+        });
+      });
+    }
   });
 
   // trigger popovers
